54c6d5da7cf6f5a703cf42321ddb2af9b7d8b796
RatchaphonHeart <132637792+RatchaphonHeart@users.noreply.github.com>
2023-05-16 09:02:01 +0000

    finished

--- .ccls-cache/@home@runner@Pointer-Exampls1/main.cpp
+++ .ccls-cache/@home@runner@Pointer-Exampls1/main.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 int main(){
-
   int a;
   double d;
   cout<<&a<<" size "<<sizeof(a) <<endl;
--- main.cpp
+++ main.cpp
@@ -1,40 +1,39 @@
+
 #include <iostream>
+#include <string.h>
+#include <iomanip>
+
+
+
 using namespace std;
 
-int main(){
+int main(void){
+  char *pa, *pb, temp;
+  char a[1000];
+  cout<<"Input String:";
+  cin>>a;
+  int lens = strlen(a);
+  pa = &a[0];
+  cout<< "Original: ";
+  for (int i = 0; i < lens - 1; i++, pa++){
+    cout<<setw(3)<<*pa<<" ";
+  }
+  cout<<setw(3)<<*pa<<endl;
 
-  int a;
-  double d;
-  cout<<&a<<" size "<<sizeof(a) <<endl;
-  cout<<&d<<" size "<<sizeof(d) <<endl; 
+    pa = &a[0]; pb = &a[lens - 1];
+    cout<<"Reverse: ";
+    for (int i = 0; i < lens / 2; i++) {
+        temp = *pa;
+        *pa = *pb;
+        *pb = temp;
+        pa++; pb--;
+    }
+    pa = &a[0];
+    for (int i = 0; i <lens - 1; i++,pa++){
+        cout<<setw(3)<<*pa<<" ";}
+       cout<<setw(3)<<*pa<<endl;
+    
+    
+return 0;
   
-  /*  int x,*p;
-    p=&x;
-    x=5;
-
-    cout<< x  <<" "<< *p <<endl;
-    cout<< &x <<" "<<  p <<endl;
-    cout<< &p <<endl;*/
-
-/*Example Array*/
-  /*
-int *ip;
-int a[ ] = {31415, 21828};
-ip = a;
-// alternative:	ip = &a[0];
-
-cout<<a<<endl;			
-cout<<ip<<" "<<*ip<<endl;	
-  
-(*ip)++; 
-cout<<a<<" "<<a[0]<<endl;		
-cout<<ip<<" "<<*ip<<endl; 	
-  
-ip++; 
-cout<<a<<endl;		
-cout<<ip<<" "<<*ip<<endl; 	// print 
-*/
- /*the address is 4324, not 4321
- because the pointer is incremented 						
- by the size of an integer (4 bytes) */
 }
